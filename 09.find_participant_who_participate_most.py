636e7855e06d1ddfcb3d6382886ac1e1be871834
iqbaldiit <iqbaldiit@gmail.com>
2025-04-02 21:18:52 +0600

    find participant

--- 09.find_participant_who_participate_most.py
+++ 09.find_participant_who_participate_most.py
@@ -26,11 +26,17 @@
 
     Solution Explanation: The problem stated that, we have to find out who participate most rank 1.
 
-    Approach:
+    Approach (using explode):
         1. explode the rank column
         2. count of rank where rank=1
-        3. find max of the count
-        4. print name of the result.
+        3. find max of the count  or use DENSE_RANK
+        4. filter and print name.
+
+    Approach (Using Size function)
+        1. get the size of the rank array where rank=1
+        2. find max count or use DENSE_RANK
+        3. match and print
+
 '''
 from spark_session import *
 from pyspark.sql.functions import *
@@ -59,22 +65,41 @@ print("==========Expected output=============")
 
 # # # # #### ================ Approach->1 : (using Explode (DSL))
 
-df=df.withColumn("rank",explode(col("rank")))
-df=df.filter(col("rank")==1)
-df=df.groupBy(col("name")).agg(count(col("rank")).alias("rk_count"))
-df=df.orderBy(col("rk_count").desc()).limit(1).select("name")
+# df=df.withColumn("rank",explode(col("rank")))
+# df=df.filter(col("rank")==1)
+# df=df.groupBy(col("name")).agg(count(col("rank")).alias("rk_count"))
+# max_rk=df.agg(max(col("rk_count")).alias("max_rk")).collect()[0][0]
+# df=df.filter(col("rk_count")==max_rk).select("name")
+# df.show()
 
+# # # # #### ================ Approach->2 : (using size and filter (DSL))
+df=df.withColumn("rk_count",expr("size(filter(rank,x->x=1))")) # get count of 1
+max_rk=df.agg(max(col("rk_count")).alias("max_rk")).collect()[0][0] # find max rank count
+df=df.filter(col("rk_count")==max_rk).select("name")
 df.show()
 
-# # # #### ================ Approach->2 : (using explode (SQL))
-
+# # # # #### ================ Approach->3 : (using explode (SQL))
+#
 # df.createOrReplaceTempView("Participants")
 # df=spark.sql("WITH cte AS ("
 #              " SELECT name,rk FROM Participants"
 #              " LATERAL VIEW explode(rank) as rk )"
-#              " SELECT name FROM"
-#              " (SELECT name, COUNT(rk) rk_count FROM cte WHERE rk=1"
-#              " GROUP BY name)tab ORDER BY rk_count DESC LIMIT (1)"
+#              " SELECT tab2.name FROM ("
+#              " SELECT tab.name, DENSE_RANK() OVER (ORDER BY tab.rk_count DESC) crk FROM ("
+#              " SELECT name, COUNT(rk) rk_count FROM cte WHERE rk=1"
+#              " GROUP BY name "
+#              " )tab)tab2 WHERE tab2.crk=1"
+# )
+#
+# df.show()
+
+# # # # # #### ================ Approach->4 : (size and filter (SQL))
+# #
+# df.createOrReplaceTempView("Participants")
+# df=spark.sql(" SELECT name FROM ("
+#              " SELECT name, DENSE_RANK() OVER (ORDER BY rk_count DESC) AS crk FROM ("
+#              " SELECT *, SIZE(FILTER(rank, x->x=1)) AS rk_count FROM Participants"
+#              " )tab)tab2 WHERE crk=1"
 # )
 #
 # df.show()
