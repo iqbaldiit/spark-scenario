e64edd894a56ff90ce38773064087ae84cee8921
iqbaldiit <iqbaldiit@gmail.com>
2026-09-25 18:50:39 +0600

    Find Employee Whose salary incremented

--- LeetCode_spark/18.find-users-with-persistent-behavior-patterns.py
+++ LeetCode_spark/18.find-users-with-persistent-behavior-patterns.py
@@ -123,50 +123,50 @@ df.show()
 print()
 print("==========Expected output=============")
 
-# # #  # # # # # #### ================ Approach->1 : (DSL)
-#
-# win_lead=Window.partitionBy("user_id","action").orderBy("user_id","action","action_date")
-# win_count=Window.partitionBy("user_id","action_date")
-#
-# df=(df.withColumn("next_date",lead("action_date").over(win_lead))
-#     .withColumn("action_count",count("*").over(win_count))
-#     .withColumn("date_diff",datediff(col("next_date"),col("action_date")))
-#     )
-#
-# df=(df.groupBy("user_id","action","action_count","date_diff").agg(
-#     (1+sum("date_diff")).alias("streak_length")
-#     ,min("action_date").alias("start_date")
-#     ,max("next_date").alias("end_date")
-# ).where((col("action_count")==1)
-#         & (col("date_diff")==1)
-#         & (col("streak_length")>=5))
-#     .select("user_id","action","streak_length","start_date","end_date")
-#     .orderBy(desc("streak_length"),asc("user_id"))
-# )
-#
-# df.show()
+# #  # # # # # #### ================ Approach->1 : (DSL)
 
-# # # #### ================ Approach->2 : (SQL)
-df.createOrReplaceTempView("activity")
+win_lead=Window.partitionBy("user_id","action").orderBy("user_id","action","action_date")
+win_count=Window.partitionBy("user_id","action_date")
 
-sSQL="""
-    WITH tbl_lead AS (
-    SELECT *
-    , LEAD(action_date) OVER (PARTITION BY user_id,action ORDER BY user_id,action,action_date) AS next_date
-    , COUNT(action) OVER (PARTITION BY user_id,action_date) AS action_count
-    FROM activity 
+df=(df.withColumn("next_date",lead("action_date").over(win_lead))
+    .withColumn("action_count",count("*").over(win_count))
+    .withColumn("date_diff",datediff(col("next_date"),col("action_date")))
     )
-    SELECT user_id,action
-    ,SUM(DATEDIFF(DAY,action_date,next_date))+1 AS streak_length 
-    ,MIN(action_date) start_date
-    ,MAX(next_date) end_date
-    FROM tbl_lead WHERE action_count=1 AND DATEDIFF(DAY,action_date,next_date)=1
-    GROUP BY user_id,action
-    HAVING SUM(DATEDIFF(DAY,action_date,next_date))+1>=5
-    ORDER BY streak_length DESC, user_id ASC
-"""
-df=spark.sql(sSQL)
+
+df=(df.groupBy("user_id","action","action_count","date_diff").agg(
+    (1+sum("date_diff")).alias("streak_length")
+    ,min("action_date").alias("start_date")
+    ,max("next_date").alias("end_date")
+).where((col("action_count")==1)
+        & (col("date_diff")==1)
+        & (col("streak_length")>=5))
+    .select("user_id","action","streak_length","start_date","end_date")
+    .orderBy(desc("streak_length"),asc("user_id"))
+)
+
 df.show()
+
+# # # # #### ================ Approach->2 : (SQL)
+# df.createOrReplaceTempView("activity")
+#
+# sSQL="""
+#     WITH tbl_lead AS (
+#     SELECT *
+#     , LEAD(action_date) OVER (PARTITION BY user_id,action ORDER BY user_id,action,action_date) AS next_date
+#     , COUNT(action) OVER (PARTITION BY user_id,action_date) AS action_count
+#     FROM activity
+#     )
+#     SELECT user_id,action
+#     ,SUM(DATEDIFF(DAY,action_date,next_date))+1 AS streak_length
+#     ,MIN(action_date) start_date
+#     ,MAX(next_date) end_date
+#     FROM tbl_lead WHERE action_count=1 AND DATEDIFF(DAY,action_date,next_date)=1
+#     GROUP BY user_id,action
+#     HAVING SUM(DATEDIFF(DAY,action_date,next_date))+1>=5
+#     ORDER BY streak_length DESC, user_id ASC
+# """
+# df=spark.sql(sSQL)
+# df.show()
 
 
 
